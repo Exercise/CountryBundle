d802934377bb61e0bc18f8844089893580f2b626
Luis Cordova <cordoval@gmail.com>
2011-06-14 14:57:05 -0500

    added Country.php transferred ODM->ORM details ran php app/console doctrine:generate:entities Bundle/CountryBundle

---
+++ Entity/Country.php
@@ -0,0 +1,152 @@
+<?php
+namespace Bundle\ExerciseCom\CountryBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="Bundle\ExerciseCom\CountryBundle\Entity\CountryRepository")
+ *
+ * @ORM\Table(name="country")
+ *
+ */
+class Country
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     *
+     * @var integer $id
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", unique=true, length="10")
+     *
+     * @Assert\NotBlank(message="Please enter country code", groups="CreateCountry")
+     * @Assert\MaxLength(limit="10", message="Try shortening country code", groups="CreateCountry")
+     *
+     * @var string $code
+     */
+    protected $code;
+
+    /**
+     * @ORM\Column(type="string", unique=true, length="32")
+     *
+     * @Assert\NotBlank(message="Please enter country name", groups="CreateCountry")
+     * @Assert\MaxLength(limit="32", message="Try shortening country name", groups="CreateCountry")
+     *
+     * @var string $name
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="string", unique=true, length="3")
+     * @Assert\NotBlank(message="Please enter ISO3 country code", groups="CreateCountry")
+     *
+     * @var string $iso3
+     */
+    protected $iso3;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $rank;
+
+    public function __toString()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string $code
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string $name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set iso3
+     *
+     * @param string $iso3
+     */
+    public function setIso3($iso3)
+    {
+        $this->iso3 = $iso3;
+    }
+
+    /**
+     * Get iso3
+     *
+     * @return string $iso3
+     */
+    public function getIso3()
+    {
+        return $this->iso3;
+    }
+
+    /**
+     * Set rank
+     *
+     * @param integer $rank
+     */
+    public function setRank($rank)
+    {
+        $this->rank = $rank;
+    }
+
+    /**
+     * Get rank
+     *
+     * @return integer $rank
+     */
+    public function getRank()
+    {
+        return $this->rank;
+    }
+}
