8794e579e6b126b43c95a93f4f00567bf66382da
Jeremy Mikola <jmikola@gmail.com>
2011-10-06 19:35:00 -0400

    Fix references to CountryBundle resources

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -9,17 +9,17 @@ class BackendController extends BaseController
 {
     public function indexAction()
     {
-        return $this->get('templating')->renderResponse('Country:Backend:index.html.twig');
+        return $this->get('templating')->renderResponse('CountryBundle:Backend:index.html.twig');
     }
 
     public function newAction()
     {
-        return $this->get('templating')->renderResponse('Country:Backend:new.html.twig');
+        return $this->get('templating')->renderResponse('CountryBundle:Backend:new.html.twig');
     }
 
     public function createAction()
     {
-        return $this->get('templating')->renderResponse('Country:Backend:create.html.twig');
+        return $this->get('templating')->renderResponse('CountryBundle:Backend:create.html.twig');
     }
 
     public function editAction($identifier)
@@ -30,7 +30,7 @@ class BackendController extends BaseController
             throw new NotFoundHttpException();
         }
 
-        return $this->get('templating')->renderResponse('Country:Backend:edit.html.twig', array('object' => $object));
+        return $this->get('templating')->renderResponse('CountryBundle:Backend:edit.html.twig', array('object' => $object));
     }
 
     public function updateAction($identifier)
